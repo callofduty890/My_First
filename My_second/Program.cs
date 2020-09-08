8b2b62bbb5e4d2d82b75ec56cc8d4f0a561b357a
俊华 蔡 <1032031912@qq.com>
2020-09-08 11:27:24 +0800

    添加了变量

---
+++ My_second/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //整数
+            int p = 3;
+            //浮点数/小数
+            double s = 3.14;
+            //字符串类型
+            string str = "超人视觉";
+            //字节
+            char c = 'a';
+
+            //输出内容
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+        }
+    }
+}
--- VS19_My_First/Program.cs
+++ VS19_My_First/Program.cs
@@ -8,13 +8,20 @@ namespace VS19_My_First
 {
     class Program
     {
+        /// <summary>
+        /// 主入口函数
+        /// </summary>
+        /// <param name="args">数组</param>
         static void Main(string[] args)
         {
             //输出
             Console.WriteLine("这是我的第一个程序!");
             //占位符
+            /*
             Console.WriteLine("这是我的第{0}个程序!", "二");
             Console.WriteLine("这是我的第{0}个程序!,我感觉{1}", "二","非常好");
+            */
+            //Console.WriteLine("");
             Console.ReadKey();
         }
 
