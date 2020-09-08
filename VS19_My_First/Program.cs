2eafae4cb5487f74b07a597dd2278148fef6f8c2
俊华 蔡 <1032031912@qq.com>
2020-09-08 10:29:37 +0800

    添加了占位符

--- VS19_My_First/Program.cs
+++ VS19_My_First/Program.cs
@@ -10,8 +10,11 @@ namespace VS19_My_First
     {
         static void Main(string[] args)
         {
-            //
+            //输出
             Console.WriteLine("这是我的第一个程序!");
+            //占位符
+            Console.WriteLine("这是我的第{0}个程序!", "二");
+            Console.WriteLine("这是我的第{0}个程序!,我感觉{1}", "二","非常好");
             Console.ReadKey();
         }
 
