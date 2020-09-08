ea20eff66b1ba636ca658350c9e43595770e911c
俊华 蔡 <1032031912@qq.com>
2020-09-08 10:19:39 +0800

    添加项目文件。

---
+++ VS19_My_First/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS19_My_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是我的第一个程序!");
+            Console.ReadKey();
+        }
+
+    }
+}
